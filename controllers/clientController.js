887482fefeebcadbd634c746e49396431a4f0234
Mugeha <mugehajacky@gmail.com>
2025-04-25 14:38:49 +0300

    Console logging clientId

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -29,6 +29,8 @@ exports.getClientById = async (req, res) => {
     const client = await Client.findById(req.params.id).populate('enrolledPrograms');
     if (!client) return res.status(404).json({ message: 'Client not found' });
     res.status(200).json(client);
+    console.log('Client ID from params:', req.params.id);
+    
   } catch (err) {
     res.status(500).json({ message: 'Error fetching client profile', error: err.message });
   }
