d6816d14d305e9757c0a0c9549438de9ade8f0af
Mugeha <mugehajacky@gmail.com>
2025-05-06 16:00:48 +0300

    Configuring tailwindcss

--- health-info-frontend/tailwind.config.js
+++ health-info-frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -7,4 +7,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+}
