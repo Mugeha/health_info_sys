9f37bedc40b1b5818e7f712de4a16d727a596c27
Mugeha <mugehajacky@gmail.com>
2025-05-20 19:20:22 +0300

    Added debugging logs for TypeError: argument handler must be a function

--- routes/analytics.js
+++ routes/analytics.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 const { getClientAnalyticsSummary } = require('../controllers/analyticsController');
+console.log('DEBUG: getClientAnalyticsSummary is:', getClientAnalyticsSummary);
+console.log('DEBUG: protect is:', typeof protect);
+console.log('DEBUG: getClientAnalyticsSummary is:', typeof getClientAnalyticsSummary);
+
+
 
 router.get('/summary', protect, getClientAnalyticsSummary);
 
