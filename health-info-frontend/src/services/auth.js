b33e30f4f438f16edb25726164d7c4af4c3a3ade
Mugeha <mugehajacky@gmail.com>
2025-05-21 17:39:23 +0300

    Change from loginDoctor to loginUser

--- health-info-frontend/src/services/auth.js
+++ health-info-frontend/src/services/auth.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export const loginDoctor = async (credentials) => {
+export const loginUser = async (credentials) => {
   const res = await axios.post('http://localhost:5000/api/auth/login', credentials);
 
   // Store token and role in localStorage
   localStorage.setItem('token', res.data.token);
-  localStorage.setItem('role', res.data.user.role); // <- store role for frontend use
+  localStorage.setItem('role', res.data.user.role); // <- for role-based UI
 
   return res.data;
 };
