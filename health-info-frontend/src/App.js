a0f0918096ba3c452f61dad4a781bb46edb517fe
Mugeha <mugehajacky@gmail.com>
2025-05-22 21:23:52 +0300

    Updated app.js

--- health-info-frontend/src/App.js
+++ health-info-frontend/src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import { Toaster } from 'react-hot-toast'; // ✅ import toaster
+import { Toaster } from 'react-hot-toast';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import Analytics from './pages/Analytics'; // 🔥 Add this import
+import Analytics from './pages/Analytics';
 import Clients from './pages/Clients';
 import AddClient from './pages/AddClient';
 import ClientProfile from './pages/ClientProfile';
@@ -13,74 +13,35 @@ import EnrollClient from './pages/EnrollClient';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import ClientSearch from './pages/ClientSearch';
+import LoginRedirectGuard from './components/LoginRedirectGuard'; // ✅ new import
 
 function App() {
   return (
     <Router>
-      {/* ✅ Toaster needs to be inside Router but outside Routes */}
       <Toaster position="top-center" reverseOrder={false} />
 
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
 
+        {/* ✅ Redirect logged-in users from login */}
         <Route
-          path="/dashboard"
+          path="/login"
           element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
+            <LoginRedirectGuard>
+              <Login />
+            </LoginRedirectGuard>
           }
         />
-        <Route
-          path="/clients"
-          element={
-            <PrivateRoute>
-              <Clients />
-            </PrivateRoute>
-          }
-        />
+
+        {/* Private Routes */}
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/clients" element={<PrivateRoute><Clients /></PrivateRoute>} />
+        <Route path="/analytics" element={<PrivateRoute><Analytics /></PrivateRoute>} />
+        <Route path="/add-client" element={<PrivateRoute><AddClient /></PrivateRoute>} />
+        <Route path="/clients/:id" element={<PrivateRoute><ClientProfile /></PrivateRoute>} />
+        <Route path="/programs" element={<PrivateRoute><Programs /></PrivateRoute>} />
+        <Route path="/clients/:id/enroll" element={<PrivateRoute><EnrollClient /></PrivateRoute>} />
         
-<Route
-  path="/analytics"
-  element={
-    <PrivateRoute>
-      <Analytics />
-    </PrivateRoute>
-  }
-/>
-        <Route
-          path="/add-client"
-          element={
-            <PrivateRoute>
-              <AddClient />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/clients/:id"
-          element={
-            <PrivateRoute>
-              <ClientProfile />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/programs"
-          element={
-            <PrivateRoute>
-              <Programs />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/clients/:id/enroll"
-          element={
-            <PrivateRoute>
-              <EnrollClient />
-            </PrivateRoute>
-          }
-        />
         <Route path="/client-search" element={<ClientSearch />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
