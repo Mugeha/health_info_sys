db3960e2122135242ce9f7e0e1d4ffd5a1dccc0b
Mugeha <mugehajacky@gmail.com>
2025-05-27 16:24:31 +0300

    More changes to facilitate logging in with email

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,7 @@ exports.loginUser = async (req, res) => {
 const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ username });
+const user = await User.findOne({ email });
 
     if (!user || !(await user.comparePassword(password))) {
       return res.status(401).json({ message: 'Invalid credentials' });
