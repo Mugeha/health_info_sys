6cb4c536bc8e2a36fe383f89b1e44752bd074097
Mugeha <mugehajacky@gmail.com>
2025-06-06 20:40:10 +0300

    Applying the rate limiter middleware to the my forgot password route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { registerDoctor, loginUser,forgotPassword,
   resetPassword } = require('../controllers/authController');
+const { forgotPasswordLimiter } = require('../middleware/rateLimiter');
 
 const router = express.Router();
 
 router.post('/register', registerDoctor);
 router.post('/login', loginUser);
-router.post('/forgot-password', forgotPassword);
+router.post('/forgot-password', forgotPasswordLimiter, forgotPassword);
 router.post('/reset-password/:token', resetPassword);
 
 
